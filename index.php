dc112c66d3aadbe098e41d01721a4e3b9f64c6de
rbtzz <liera@outlook.com>
2013-06-09 01:48:35 -0500

    carpeta completa del proyecto
    todos los archivos necesarios para el proyecto

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<html>
+<!-- ********************************************************************* -->
+<head>
+	  	<meta charset="UTF-8">  <!-- Conjunto de tipos de caractereres aceptables-->
+	   	<meta name="description" content="Ejemplo de HTML5" />
+  		<meta name="keywords" content="HTML5, CSS3, JavaScript" />
+  		<title>SISCABAR</title>
+  		<link rel="stylesheet" type="text/css"  href="css/stylesiscabar.css" media="all" >
+  		<link rel="shortcut icon" type="image/jpg" href="1.jpg">
+</head>
+
+<!-- ********************************************************************* -->
+<body>
+
+  <div id="agrupar">
+
+  	<section id="seccion">
+
+  		<article class="encabezado">
+  		    <figure>
+            <img src="siscabarlog.png"  width='606' height='200'>
+          </figure>
+  		</article>
+
+  		<article class="form">
+          		<h1>SISTEMA DE CONTROL DE BARES</h1>
+
+          		<form action="index.php" id="login_usuarios.php" method="post">
+            		<label id="style_02">Usuario:</label>
+               	<input type="text" id="style_01"name="usuario" autofocus placeholder="Escribe tu usuario">
+                <label id="style_02">Password:</label>
+                <input type="password" id="style_01"name="password" autofocus placeholder="Escribe tu password">
+                <input type="submit" id="style_02" value="INICIAR SESION">
+              </form>
+      </article>
+  	</section>
+
+  </div>
+<footer id="pie"></footer>
+</body>
+</html>
